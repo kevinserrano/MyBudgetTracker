a8779e046e768e6e859e387f221cb28e9d24f70a
kevinserrano <66225834+kevinserrano@users.noreply.github.com>
2020-11-24 16:24:07 -0600

    trying heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const FILES_TO_CACHE = [
+const cacheFiles = [
   '/',
   '/styles.css',
   '/index.html',
@@ -9,28 +9,28 @@ const FILES_TO_CACHE = [
   '/icons/icon-512x512.png'
 ];
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+const cacheName = "static-cache-v2";
+const dataCacheName = "data-cache-v1";
 
 // install
-self.addEventListener("install", function(evt) {
-evt.waitUntil(
-  caches.open(CACHE_NAME).then(cache => {
+self.addEventListener("install", function(event) {
+event.waitUntil(
+  caches.open(cacheName).then(cache => {
     console.log("Your files were pre-cached successfully!");
-    return cache.addAll(FILES_TO_CACHE);
+    return cache.addAll(cacheFiles);
   })
 );
 
-self.skipWaiting();
+
 });
 
 // activate
-self.addEventListener("activate", function(evt) {
-evt.waitUntil(
+self.addEventListener("activate", function(event) {
+event.waitUntil(
   caches.keys().then(keyList => {
     return Promise.all(
       keyList.map(key => {
-        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+        if (key !== cacheName && key !== dataCacheName) {
           console.log("Removing old cache data", key);
           return caches.delete(key);
         }
@@ -43,32 +43,32 @@ self.clients.claim();
 });
 
 // fetch
-self.addEventListener("fetch", function(evt) {
-if (evt.request.url.includes("/api/")) {
-  evt.respondWith(
-    caches.open(DATA_CACHE_NAME).then(cache => {
-      return fetch(evt.request)
+self.addEventListener("fetch", function(event) {
+if (event.request.url.includes("/api/")) {
+  event.respondWith(
+    caches.open(dataCacheName).then(cache => {
+      return fetch(event.request)
         .then(response => {
           // If the response was good, clone it and store it in the cache.
           if (response.status === 200) {
-            cache.put(evt.request.url, response.clone());
+            cache.put(event.request.url, response.clone());
           }
 
           return response;
         })
         .catch(err => {
           // Network request failed, try to get it from the cache.
-          return cache.match(evt.request);
+          return cache.match(event.request);
         });
     }).catch(err => console.log(err))
   );
 
   return;
 }
-evt.respondWith(
-caches.open(CACHE_NAME).then(cache => {
-  return cache.match(evt.request).then(response => {
-    return response || fetch(evt.request);
+event.respondWith(
+caches.open(cacheName).then(cache => {
+  return cache.match(event.request).then(response => {
+    return response || fetch(event.request);
   });
 })
 );
